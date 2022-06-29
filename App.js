d1530c45039ee61b4802e856056e67bd9c40f80c
mrazadtamal <mrazadt@gmail.com>
2022-06-29 16:52:13 +0600

    HomeScreen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>azad</Text>
+      <HomeScreen />
+
       <StatusBar style="auto" />
     </View>
   );

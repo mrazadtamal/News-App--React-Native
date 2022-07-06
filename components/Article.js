da67bbef8f0a3bfa48196a4bfe841551c91cb29e
mrazadtamal <mrazadt@gmail.com>
2022-07-06 23:23:40 +0600

    done

--- components/Article.js
+++ components/Article.js
@@ -12,16 +12,22 @@ const Article = () => {
   return (
     <ScrollView>
       <SafeAreaView style={styles.container}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: "https://res.cloudinary.com/abidazad/image/upload/v1656176833/man-4594401_1280_pgdx7g.jpg",
+          }}
+        />
         <View>
-          <Text>Azad</Text>
+          <Text style={styles.title}>Azad</Text>
         </View>
         <View>
-          <Image
-            style={styles.image}
-            source={{
-              uri: "https://res.cloudinary.com/abidazad/image/upload/v1656176833/man-4594401_1280_pgdx7g.jpg",
-            }}
-          />
+          <Text style={styles.description}>
+            In publishing and graphic design, Lorem ipsum is a placeholder text
+            commonly used to demonstrate the visual form of a document or a
+            typeface without relying on meaningful content. Lorem ipsum may be
+            used as a placeholder
+          </Text>
         </View>
       </SafeAreaView>
     </ScrollView>
@@ -38,6 +44,16 @@ const styles = StyleSheet.create({
     height: 200,
     width: "100%",
     borderRadius: 8,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "600",
+    marginTop: 10,
+  },
+  description: {
+    fontSize: 15,
+    fontWeight: "400",
+    marginTop: 10,
   },
 });
 
